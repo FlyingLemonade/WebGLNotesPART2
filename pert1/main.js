69b7efc906938cdb0e4ac385a8b8c88e781b0d22
Nikolas Henrik Owen <128313978+NikoHoc@users.noreply.github.com>
2024-05-06 18:20:49 +0700

    ADA SATELITE WAH

--- pert1/main.js
+++ pert1/main.js
@@ -1,5 +1,7 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 
 //setup canvas renderer
 const renderer = new THREE.WebGLRenderer();
@@ -8,12 +10,17 @@ document.body.appendChild(renderer.domElement);
 
 //setup scene and camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 10);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(0, 0, 0)
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
 controls.update();
 
 //Add Box
@@ -21,27 +28,26 @@ controls.update();
 
 //sun
 var geometry = new THREE.SphereGeometry(1, 10, 10);
-var material = new THREE.MeshPhongMaterial({color: 0xFFFF00})
+var material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
 var sun = new THREE.Mesh(geometry, material);
-scene.add(sun)
+scene.add(sun);
 
 //Earth
 var earthGeo = new THREE.SphereGeometry(1, 10, 10);
-var earthMaterial = new THREE.MeshPhongMaterial({color: 0x3333ff})
-var earth = new THREE.Mesh(earthGeo, earthMaterial)
-sun.add(earth)
-earth.position.set(5, 0, 0)
-earth.scale.set(0.5, 0.5, 0.5)
-earth.rotation.x+=0.3
+var earthMaterial = new THREE.MeshPhongMaterial({ color: 0x3333ff });
+var earth = new THREE.Mesh(earthGeo, earthMaterial);
+sun.add(earth);
+earth.position.set(5, 0, 0);
+earth.scale.set(0.5, 0.5, 0.5);
+earth.rotation.x += 0.3;
 
 //Moon
 var moonGeo = new THREE.SphereGeometry(1, 10, 10);
-var moonMaterial = new THREE.MeshPhongMaterial({color: 0x55555})
+var moonMaterial = new THREE.MeshPhongMaterial({ color: 0x55555 });
 var moon = new THREE.Mesh(moonGeo, moonMaterial);
-earth.add(moon)
-moon.position.set(5, 0, 0)
-moon.scale.set(0.25, 0.25, 0.25)
-
+earth.add(moon);
+moon.position.set(5, 0, 0);
+moon.scale.set(0.25, 0.25, 0.25);
 
 // //Ambient Light
 // //agar ambient light bisa, ubah di bagian material -> .MeshBasicMaterial menjadi -> .MeshPhongMaterial
@@ -50,38 +56,50 @@ moon.scale.set(0.25, 0.25, 0.25)
 
 //Hemisphere Light
 //sky color, ground color, intensitas
-var hemisphereLight = new THREE.HemisphereLight(0xB1E1FF, 0xB97A20, 0.5)
-scene.add(hemisphereLight)
+var hemisphereLight = new THREE.HemisphereLight(0xb1e1ff, 0xb97a20, 0.5);
+scene.add(hemisphereLight);
 
 //Directional Light
-var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10)
-directionalLight.position.set(3, 3, 3)
+var directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+directionalLight.position.set(3, 3, 3);
 directionalLight.target.position.set(0, 0, 0);
-scene.add(directionalLight)
-scene.add(directionalLight.target)
+scene.add(directionalLight);
+scene.add(directionalLight.target);
 
 // menunjukan arah cahaya dari mana
-var directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+var directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 scene.add(directionalLightHelper);
 
-
 //Point light
-var pointLight = new THREE.PointLight(0xFFFF11, 150)
+var pointLight = new THREE.PointLight(0xffff11, 150);
 sun.add(pointLight);
 
-
 //spot light
-var spotLight = new THREE.SpotLight(0xFF1111, 200, 1000, 15, 10)
+var spotLight = new THREE.SpotLight(0xff1111, 200, 1000, 15, 10);
 moon.add(spotLight);
-earth.add(spotLight.target)
+earth.add(spotLight.target);
 
-moon.add(new THREE.SpotLightHelper(spotLight))
+moon.add(new THREE.SpotLightHelper(spotLight));
+
+new MTLLoader()
+  .setPath("resources/Satellite/")
+  .load("Satelite.mtl", function (materials) {
+    materials.preload();
+    new OBJLoader()
+      .setMaterials(materials)
+      .setPath("resources/Satellite/")
+      .load("satelite.obj", function (object) {
+        earth.add(object);
+        object.scale.set(0.1, 0.1, 0.1);
+        object.position.set(-3, 0, 0);
+      });
+  });
 
 //Loop Animate
 function animate() {
-    renderer.render(scene, camera)
-    sun.rotation.y += 0.01
-    earth.rotation.y += 0.05
-    requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  sun.rotation.y += 0.01;
+  earth.rotation.y += 0.05;
+  requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
